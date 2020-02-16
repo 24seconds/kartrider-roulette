8e4b428c3af28db5f4f94b774e8cdc7e2de81757
GeunYeong Cheon <24crazyoung@gmail.com>
2020-02-16 16:59:00 +0900

    Add difficulty, reverse, crazy UI in TrackItemComponent

--- src/component/TrackPopup/TrackItemComponent.js
+++ src/component/TrackPopup/TrackItemComponent.js
@@ -2,16 +2,55 @@ import React, { Component } from 'react';
 import { IMAGE_URL } from '../../redux/store';
 
 export default class TrackItemComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dotArray: [0,1,2,3,4]
+    };
+  }
+
   render() {
     const { track = null } = this.props;
+    const { dotArray } = this.state;
 
     return (
       track &&
       <div className='kartrider-track-item-component'>
         {/* selected or not */}
         <div className='border'>
+          <div className='track-difficulty'>
+            <div className='text'>
+              난이도
+            </div>
+            <div className='dot-container'>
+              {
+                dotArray.map(i => {
+                  if (i <= track['difficulty'] - 1) {
+                    return (
+                      <div className='dot on'></div>
+                    );
+                  } else {
+                    return (
+                      <div className='dot'></div>
+                    );
+                  }
+                })
+              }
+            </div>
+          </div>
+          { track['isReverse'] &&
+            <div className='special reverse'>
+              Reverse
+            </div>
+          }
+          { track['isCrazy'] &&
+            <div className='special crazy'>
+              Crazy
+            </div>
+          }
           <div className='track-image' >
-            <img src={ `${IMAGE_URL}/track/${ track['displayName'] }.png` } alt="track icon" />
+            <img src={ `${IMAGE_URL}/track/${ track['imageName'] }.png` } alt="track icon" />
           </div>
         </div>
         <div className='track-name'>
