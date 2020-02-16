c3434b53cd6fea2557a3520a76d251452ec3287b
GeunYeong Cheon <24crazyoung@gmail.com>
2020-02-16 16:50:09 +0900

    Rename displaName into imageName in track_data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
 import rootReducer from "./reducer";
 
-export const IMAGE_URL = 'asset'
+export const IMAGE_URL = 'asset';
 export default createStore(rootReducer);
