1052c5994d87b8a24900ac3093f044a5d99c42bb
Ho <cgh100039@gmail.com>
2025-02-27 11:59:55 +0900

    feat: Add search functionality based on trackName

--- src/component/Collection/CollectionDetailItemComponent.js
+++ src/component/Collection/CollectionDetailItemComponent.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import { IMAGE_URL, ROULETTE_COLLECTION_ID } from '../../database/constant';
 import IndexedDbManager from '../../database/IndexedDbManager';
+import hansearch from "hangul-search";
 
 export default class CollectionDetailItemComponent extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       trackList: [],
       themeList: [],
+      mapInputValue : '',
     }
-
     this.onBack = this.onBack.bind(this);
     this.onDeleteItem = this.onDeleteItem.bind(this);
     this.getTrackLength = this.getTrackLength.bind(this);
+    this.handleSearchMapNameOnChange = this.handleSearchMapNameOnChange.bind(this);
   }
 
   onBack() {
     this.props.onClick(false, null);
+    this.state.mapInputValue = '';
   }
 
   onDeleteItem(trackName) {
@@ -56,16 +58,28 @@ export default class CollectionDetailItemComponent extends Component {
     }
   }
 
+
+  handleSearchMapNameOnChange(e) {
+    this.setState({ mapInputValue: e.target.value });
+  }
+
   render() {
     const { trackList, collectionId } = this.props;
     const { themeList } = this.state;
-
+    const { mapInputValue } = this.state;
     return (
       <div className='kartrider-collection-detail-item-component'>
         <h3 className='collection-detail-title' >
           <div>
             { this.getDetailTitle() }
           </div>
+          <input
+            className='mapSearchInput'
+            type="text"
+            value={this.state.mapInputValue}
+            onChange={this.handleSearchMapNameOnChange}
+            placeholder="맵 이름 검색"
+          />
           <button className='collection-detail-back' onClick={ this.onBack } >
             <img src={ `${IMAGE_URL}/icon_back.png` } alt="back to collection list icon" />
           </button>
@@ -74,26 +88,52 @@ export default class CollectionDetailItemComponent extends Component {
           {
             themeList.map(theme => {
               const tracks = trackList[theme['themeName']] || [];
+              const searchTrack = hansearch(tracks,mapInputValue)
               if (!tracks.length) {
                 return null;
               }
-
               return tracks.map(track => {
-                return (
-                  <div key={ `key-detail-${track['trackName']}` } className='collection-detail-track'>
-                    <img src={ `${IMAGE_URL}/theme/${track.theme}.png` } alt="track icon" />
-                    <div>
-                      { track['trackName'] }
+                if(!mapInputValue.length){
+                  return (
+                    <div key={ `key-detail-${track['trackName']}` } className='collection-detail-track'>
+                      <img src={ `${IMAGE_URL}/theme/${track.theme}.png` } alt="track icon" />
+                      <div>
+                        { track['trackName'] }
+                      </div>
+                      {
+                        collectionId === ROULETTE_COLLECTION_ID
+                        ? null
+                        : <button className='collection-detail-delete' onClick={ this.onDeleteItem.bind(this, track['trackName']) }>
+                            <img src={ `${IMAGE_URL}/icon_delete.svg` } alt="delete icon" />
+                          </button>
+                      }
                     </div>
-                    {
-                      collectionId === ROULETTE_COLLECTION_ID
-                      ? null
-                      : <button className='collection-detail-delete' onClick={ this.onDeleteItem.bind(this, track['trackName']) }>
-                          <img src={ `${IMAGE_URL}/icon_delete.svg` } alt="delete icon" />
-                        </button>
-                    }
-                  </div>
-                );
+                  );
+                }
+
+                else{
+                  const foundSeachTrack = searchTrack.items.find(Object => Object.trackName === track.trackName)
+                  if(foundSeachTrack){
+                    return (
+                      <div key={ `key-detail-${track['trackName']}` } className='collection-detail-track'>
+                        <img src={ `${IMAGE_URL}/theme/${track.theme}.png` } alt="track icon" />
+                        <div>
+                          { track['trackName'] }
+                        </div>
+                        {
+                          collectionId === ROULETTE_COLLECTION_ID
+                          ? null
+                          : <button className='collection-detail-delete' onClick={ this.onDeleteItem.bind(this, track['trackName']) }>
+                              <img src={ `${IMAGE_URL}/icon_delete.svg` } alt="delete icon" />
+                            </button>
+                        }
+                      </div>
+                    );
+                  }
+                  else{
+                    return null
+                  }
+                }
               })
             })
           }
